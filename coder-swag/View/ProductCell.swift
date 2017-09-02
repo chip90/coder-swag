0ad7d31fa9681a0301cb3cdfaef0caf17f56d9cc
Carleton Snow <chip90@gmail.com>
2017-09-02 11:36:03 -0400

    Added collection view for product page

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Carleton Snow on 9/2/17.
+//  Copyright © 2017 Carleton Snow. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
